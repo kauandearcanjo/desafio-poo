2f1ade714d8f79d38a37198070036e76f00f78b7
Kauan de Arcanjo <kauanarcanjom@gmail.com>
2026-04-08 13:31:40 -0300

    alterações finais

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main{
     public static void main(String[] args){
 
         Cliente cliente1 = new Cliente("Kauan de Arcanjo Menezes", 12);
-        Pedido p1 = new Pedido("Pastel", 15, 2);
-        Pedido p2 = new Pedido("Pastel Doce", 19, 2);
-        Pedido p3 = new Pedido("Caldo de Cana", 5, 2);
+        Pedido p1 = new Pedido("Hambúrguer ", 19, 2);
+        Pedido p2 = new Pedido("Batata frita", 7.99, 1);
+        Pedido p3 = new Pedido("Refrigerante", 5, 2);
 
         ContaRestaurante conta1 = new ContaRestaurante(cliente1, p1, p2, p3);
 
